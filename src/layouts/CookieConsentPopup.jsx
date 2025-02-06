5b64583e2e98e27b7c7370c859e5b97e9b1b69aa
Tombal Nicolas <nicolas.tombal01@gmail.com>
2025-02-06 23:31:16 +0100

    cookie consent popup design finished

--- src/layouts/CookieConsentPopup.jsx
+++ src/layouts/CookieConsentPopup.jsx
@@ -4,14 +4,17 @@ import styles from "@/styles/layouts/cookie-consent-popup.module.css";
 export default function CookieConsentPopup() {
 	return (
 		<div className={styles.container}>
-			<p>
-				Nous utilisons des cookies pour améliorer votre expérience sur notre
-				site, analyser le trafic et personnaliser le contenu. En continuant à
-				utiliser ce site, vous acceptez notre utilisation des cookies.
+			<p className={styles.text}>
+				Nous utilisons des cookies pour améliorer votre expérience et mesurer
+				notre audience. Vous pouvez accepter ou refuser leur utilisation.
 			</p>
 			<div className={styles.buttons}>
-				<button className={roboto.className}>Refuser</button>
-				<button className={roboto.className}>Accepter</button>
+				<button className={`${styles.button} ${roboto.className}`}>
+					Refuser
+				</button>
+				<button className={`${styles.button} ${roboto.className}`}>
+					Accepter
+				</button>
 			</div>
 		</div>
 	);
